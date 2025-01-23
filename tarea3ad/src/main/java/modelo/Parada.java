11b94fb83a4649878576aa217088c97027913cb6
alumnoDAM <alumnoDAM@DESKTOP-PRRG66G>
2025-01-23 16:21:37 +0100

    cambios de alvaro

--- tarea3ad/src/main/java/modelo/Parada.java
+++ tarea3ad/src/main/java/modelo/Parada.java
@@ -11,12 +11,22 @@ public class Parada {
 	@Id
 	private Long id;
 
+	private String nombre;
+	
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	
 	
