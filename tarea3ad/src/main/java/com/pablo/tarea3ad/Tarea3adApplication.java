032d3975fd807d5e19487ca97250c3499a7f06b8
alumnoDAM <alumnoDAM@DESKTOP-PRRG66G>
2025-01-23 15:53:03 +0100

    primer commit

---
+++ tarea3ad/src/main/java/com/pablo/tarea3ad/Tarea3adApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.tarea3ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3adApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3adApplication.class, args);
+	}
+
+}
---
+++ tarea3ad/src/test/java/com/pablo/tarea3adPabloMerino/Tarea3adPabloMerinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.tarea3adPabloMerino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3adPabloMerinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
